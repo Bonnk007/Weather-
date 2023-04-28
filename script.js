c36f606de922a4da9088e5524e1b594601916a2e
Praveen kumar <118445375+Bonnk007@users.noreply.github.com>
2023-04-28 20:19:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const url =
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Chennai";
+const options = {
+  method: "GET",
+  headers: {
+    "content-type": "application/octet-stream",
+    "X-RapidAPI-Key": "107eccde6emshc633e67f3fb3694p1f8345jsn3e237b764e57",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+(async () => {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+})();
