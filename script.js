de3ad582c360a982cde55e0936c71b73d577c39b
Praveen <praveentuf15@gmail.com>
2023-05-07 23:27:22 +0530

    added response

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const url = "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=pune";
+const url =
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=delhi";
 const options = {
   method: "GET",
   headers: {
@@ -8,12 +9,22 @@ const options = {
   },
 };
 
-(async () => {
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
-})();
+fetch(
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=pune",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    cloud_pct = response.cloud_pct;
+    temp = response.temp;
+    feels_like = response.feels_like;
+    humidity = response.humidity;
+    min_temp = response.min_temp;
+    max_temp = response.max_temp;
+    wind_speed = response.wind_speed;
+    wind_degrees = response.wind_degrees;
+    sunrise = response.sunrise;
+    sunset = response.sunset;
+  })
+  .catch((error) => console.log(error));
