ca02a8a65551d44aa8fb9ac319ccf292cb87d74f
Praveen <praveentuf15@gmail.com>
2023-05-09 21:29:51 +0530

    removed contact us option

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const options = {
 const getWeather = (city) => {
   cityName.innerHTML = city;
   fetch(
-    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=pune",
+    `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`,
     options
   )
     .then((response) => response.json())
@@ -20,20 +20,23 @@ const getWeather = (city) => {
       console.log(response);
       cloud_pct.innerHTML = response.cloud_pct;
       temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
       feels_like.innerHTML = response.feels_like;
       humidity.innerHTML = response.humidity;
+      humidity2.innerHTML = response.humidity;
       min_temp.innerHTML = response.min_temp;
       max_temp.innerHTML = response.max_temp;
       wind_speed.innerHTML = response.wind_speed;
+      wind_speed2.innerHTML = response.wind_speed;
       wind_degrees.innerHTML = response.wind_degrees;
       sunrise.innerHTML = response.sunrise;
       sunset.innerHTML = response.sunset;
     })
     .catch((error) => console.log(error));
 };
-SubmitEvent.addEventListener("click", (e) => {
+submit.addEventListener("click", (e) => {
   e.preventDefault();
   getWeather(city.value);
 });
 
-getWeather("pune");
+getWeather("");
